24bd5b1552ef22554c799ab13e632b76bbdc0b59
Ahmet Kar <118938057+AhmeetKaar@users.noreply.github.com>
2023-02-01 13:32:23 +0300

    Python İle Fonksiyonlar
    Bazı Problemlerin Çözümleri

---
+++ Fonksiyonlar/BirSayınınTamBölenleriniBulma.py
@@ -0,0 +1,29 @@
+"""
+BİR SAYININ TAM BÖLENLERİNİ BULMA
+
+"""
+
+def tam_bolen (sayı):
+
+    tam_bolenler = []
+
+    for i in range(2,sayı):
+
+        if (sayı % i == 0):
+            tam_bolenler.append(i)
+
+    return tam_bolenler
+
+while True:
+
+    sayı = input("Sayı:")
+
+    if (sayı == "q"):
+        print("Program Sonlandırılıyor")
+        break
+
+    else:
+        sayı = int(sayı)
+
+        print("Tam Bölenler:",tam_bolen(sayı))
+
